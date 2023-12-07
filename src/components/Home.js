79419b126d0d9997adabe9def0be29a9575a8e5b
LH104729 <LH104729@users.noreply.github.com>
2023-12-07 15:35:27 +0800

    Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Image from 'react-bootstrap/Image'
 import bg_img from '../img/ED08C064-DD9B-43AC-BEA8-DDCC7B719C23_1_105_c.jpeg'
 
-import profile_img from '../img/K7.JPG'
+import profile_img from '../img/K7.png'
 import './Home.css'
 
 const Home = () => {
